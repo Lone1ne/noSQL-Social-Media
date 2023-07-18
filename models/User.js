cab578d2298fc7ef520df714a31ac9b21ca913c4
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-07-18 18:45:47 -0400

    Created userSchema with necessary fields, created friendCount virtual, and initalized the user model.

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Must match an email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//create a virtual that retieves the length of the users friends array field
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+//initalize the user model
+const User = model("user", userSchema);
+
+module.exports = User;
