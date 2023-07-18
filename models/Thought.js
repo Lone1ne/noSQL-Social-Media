05726d974ed89f220bdf16ab66ea1d923189c409
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-07-18 19:06:40 -0400

    Created thoughtSchema with necessary fields, created reactionCount virtual, and initalized the thought model.

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+//create a virtual that retrieves the length of the thoughts reactions array field
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+//initialized the Thought model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
