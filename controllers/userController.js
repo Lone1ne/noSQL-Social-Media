fd1b1b8bcc8a1ef57aa4c80125a9890b25cac5c9
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-07-18 22:25:56 -0400

    Added new route to delete an existing user and their associated thoughts in userRoutes. Created and exported the deleteUser function from userController. Defined the deleteUser function to find a user by id and then associated thoughts and delete their information

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const { User, Thought } = require("../models");
 
 module.exports = {
   //get all users in the database
@@ -42,6 +42,20 @@ module.exports = {
         return res.status(404).json({ message: "No user with that id" });
       }
       res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async deleteUser(req, res) {
+    try {
+      const deletedUser = await User.findByIdAndDelete({
+        _id: req.params.userId,
+      });
+      if (!deletedUser) {
+        res.status(404).json({ message: "No user with that id" });
+      }
+      // await Thought.deleteMany({ _id: { in: deletedUser.thoughts } });
+      res.status(200).json({ message: "User and associated thoughts deleted" });
     } catch (err) {
       res.status(500).json(err);
     }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,11 +5,12 @@ const {
   getUser,
   createUser,
   updateUser,
+  deleteUser,
 } = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(getUsers).post(createUser);
 // /api/users/:userId
-router.route("/:userId").get(getUser).put(updateUser);
+router.route("/:userId").get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
